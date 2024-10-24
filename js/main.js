6007817e00785587200817cd49f6d4949dc89ead
NinaKharitonova <ninakharitonova95@email.com>
2024-10-24 17:24:33 +0200

    Esercizio finito

--- js/main.js
+++ js/main.js
@@ -21,3 +21,76 @@ Se non vi sentite particolarmente creativi, questa potrebbe essere un'implementa
 da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente
  diversa oppure simile, ma in ogni caso cercate di farla vostra.
  */
+
+//*Input
+const fullnameInput = document.getElementById("input-fullname");
+const pathKmInput = document.getElementById("input-path-km");
+const ageGroupInput = document.getElementById("input-age-group");
+
+//*Button
+const generateTicketButton = document.getElementById("generate-ticket");
+const clearFormButton = document.getElementById("clear-form");
+
+//*Output Field
+const fullnameField = document.getElementById("output-fullname");
+const rateField = document.getElementById("output-rate");
+const carriageField = document.getElementById("output-carriage");
+const codeCpField = document.getElementById("output-code-cp");
+const priceField = document.getElementById("output-price");
+
+//*Form
+const personalInfoForm = document.getElementById("personal-info-form");
+
+//*Ticket
+const ticketEl = document.getElementById("ticket");
+
+personalInfoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //*Recupero i dati dall'input
+
+  const fullname = fullnameInput.value;
+  const pathKm = pathKmInput.value;
+  const ageGroup = ageGroupInput.value;
+
+  //*Recupero i dati dal Field
+  let carriage = carriageField.value;
+  let codeCp = codeCpField.value;
+
+  //*Variabili che sersono per calcolare il numero random della carrozza e del codice Cp
+
+  carriage = Math.floor(Math.random() * 100) + 1;
+  codeCp = Math.floor(Math.random() * 10000) + 1;
+
+  //*Variabili che sersono per calcolare il prezzo
+
+  const kmRate = 0.21;
+  const fullPrice = kmRate * pathKm;
+
+  //*Calcolo dei vari sconti
+  let discountPerc = 0;
+  let rateText = "Biglietto standart";
+
+  if (ageGroup === "under18") {
+    discountPerc = 20;
+    rateText = "Biglietto under 18";
+  } else if (ageGroup === "over65") {
+    discountPerc = 40;
+    rateText = "Biglietto over 65";
+  }
+
+  let discountEur = (fullPrice * discountPerc) / 100;
+  let finalPrice = fullPrice - discountEur;
+  const finalPriceText = `&euro; ${finalPrice.toFixed(2)}`;
+
+  //*Dati del biglietto
+  fullnameField.innerHTML = fullname;
+  rateField.innerHTML = rateText;
+  priceField.innerHTML = finalPriceText;
+  carriageField.innerHTML = carriage;
+  codeCpField.innerHTML = codeCp;
+
+  //*Il biglietto appare quando faccio "genera"
+
+  ticketEl.classList.remove("d-none");
+});
